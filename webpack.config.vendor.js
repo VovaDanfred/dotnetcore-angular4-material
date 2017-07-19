9d93c673abfb795ebb0cb819d0b87d0254dd637c
Van Vo <vanvdse60837@live.com>
2017-07-20 00:19:36 +0700

    delete redundant stuffs

--- webpack.config.vendor.js
+++ webpack.config.vendor.js
@@ -6,11 +6,9 @@ const merge = require('webpack-merge');
 module.exports = (env) => {
     const isDevBuild = !(env && env.prod);
 
-    // const extractCSS = new ExtractTextPlugin('vendor.css');
-
     const extractSass = new ExtractTextPlugin({
         filename: "[name].css",
-        disable: false
+        disable: isDevBuild
     });
 
     const sharedConfig = {
@@ -33,8 +31,6 @@ module.exports = (env) => {
                 '@angular/platform-browser-dynamic',
                 '@angular/platform-browser/animations',
                 '@angular/router',
-                // 'bootstrap',
-                // 'bootstrap/dist/css/bootstrap.css',
                 'es6-shim',
                 'es6-promise',
                 'event-source-polyfill',
@@ -64,50 +60,11 @@ module.exports = (env) => {
             library: '[name]_[hash]'
         },
         plugins: [
-            // new webpack.ProvidePlugin({ $: 'jquery', jQuery: 'jquery' }), // Maps these identifiers to the jQuery package (because Bootstrap expects it to be a global variable)
             new webpack.ContextReplacementPlugin(/\@angular\b.*\b(bundles|linker)/, path.join(__dirname, './ClientApp')), // Workaround for https://github.com/angular/angular/issues/11580
             new webpack.ContextReplacementPlugin(/angular(\\|\/)core(\\|\/)@angular/, path.join(__dirname, './ClientApp')), // Workaround for https://github.com/angular/angular/issues/14898
             new webpack.IgnorePlugin(/^vertx$/) // Workaround for https://github.com/stefanpenner/es6-promise/issues/100
         ]
     };
-
-    // const clientBundleConfig = merge(sharedConfig, {
-    //     output: { path: path.join(__dirname, 'wwwroot', 'dist') },
-    //     module: {
-    //         rules: [
-    //             { test: /\.css(\?|$)/, use: extractCSS.extract({ use: isDevBuild ? 'css-loader' : 'css-loader?minimize' }) }
-    //         ]
-    //     },
-    //     plugins: [
-    //         extractCSS,
-    //         new webpack.DllPlugin({
-    //             path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
-    //             name: '[name]_[hash]'
-    //         })
-    //     ].concat(isDevBuild ? [] : [
-    //         new webpack.optimize.UglifyJsPlugin()
-    //     ])
-    // });
-
-    // const serverBundleConfig = merge(sharedConfig, {
-    //     target: 'node',
-    //     resolve: { mainFields: ['main'] },
-    //     output: {
-    //         path: path.join(__dirname, 'ClientApp', 'dist'),
-    //         libraryTarget: 'commonjs2',
-    //     },
-    //     module: {
-    //         rules: [{ test: /\.css(\?|$)/, use: ['to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize'] }]
-    //     },
-    //     entry: { vendor: ['aspnet-prerendering'] },
-    //     plugins: [
-    //         new webpack.DllPlugin({
-    //             path: path.join(__dirname, 'ClientApp', 'dist', '[name]-manifest.json'),
-    //             name: '[name]_[hash]'
-    //         })
-    //     ]
-    // });
-
 
     //SASS
     const clientBundleConfig = merge(sharedConfig, {
@@ -128,12 +85,10 @@ module.exports = (env) => {
                         fallback: "style-loader"
                     })
                 },
-                // { test: /\.css(\?|$)/, use: extractCSS.extract({ use: isDevBuild ? 'css-loader' : 'css-loader?minimize'} )}
             ]
         },
         plugins: [
             extractSass,
-            // extractCSS,
             new webpack.DllPlugin({
                 path: path.join(__dirname, 'wwwroot', 'dist', '[name]-manifest.json'),
                 name: '[name]_[hash]'
@@ -153,7 +108,6 @@ module.exports = (env) => {
         module: {
             rules: [
                 { test: /\.(css|scss)(\?|$)/, use: ['raw-loader', 'sass-loader'] },
-                // { test: /\.css(\?|$)/, use: ['to-string-loader', isDevBuild ? 'css-loader' : 'css-loader?minimize'] }
             ]
         },
         entry: { vendor: ['aspnet-prerendering'] },
